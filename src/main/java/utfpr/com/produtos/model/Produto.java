e0be9ec44c5c4cf36ad5f3cc26678c0d59ab7c2e
MisterAqua <italohyu122@gmail.com>
2024-12-17 04:49:42 -0300

    model database mongoDB

--- src/main/java/utfpr/com/produtos/model/Produto.java
+++ src/main/java/utfpr/com/produtos/model/Produto.java
@@ -1,5 +1,15 @@
 package utfpr.com.produtos.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "produtos")
 public class Produto {
-    
+    @Id
+    private String id;
+    private String nome;
+    private Double preco;
+    private String descricao;
 }
