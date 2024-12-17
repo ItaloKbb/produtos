a23bcde65ecd7a7685fb29d49ce317610780b954
MisterAqua <italohyu122@gmail.com>
2024-12-17 04:50:40 -0300

    tratativas de erros

--- src/main/java/utfpr/com/produtos/exception/ErrorHandler.java
+++ src/main/java/utfpr/com/produtos/exception/ErrorHandler.java
@@ -1,5 +1,27 @@
 package utfpr.com.produtos.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ErrorHandler {
-    
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustomException(CustomException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getMessage());
+        return new ResponseEntity<>(error, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", "Ocorreu um erro inesperado.");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
