ac1b694298020c5b1308537166110b05fa215426
MisterAqua <italohyu122@gmail.com>
2024-12-17 04:51:10 -0300

    serviço de basic auth para microserrviços

--- src/main/java/utfpr/com/produtos/client/AuthServiceClient.java
+++ src/main/java/utfpr/com/produtos/client/AuthServiceClient.java
@@ -1,5 +1,11 @@
 package utfpr.com.produtos.client;
 
-public class AuthServiceClient {
-    
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "auth-service", url = "http://localhost:9090/api")
+public interface AuthServiceClient {
+    @PostMapping("/users/validate")
+    Boolean validateBasicAuth(@RequestHeader("Authorization") String basicAuthHeader);
 }
