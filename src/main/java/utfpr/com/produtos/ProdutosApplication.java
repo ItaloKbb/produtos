791161209cbf52f9ab825bef6c0ae41bb2347a42
MisterAqua <italohyu122@gmail.com>
2024-12-17 04:50:53 -0300

    EnableFeignClients

--- src/main/java/utfpr/com/produtos/ProdutosApplication.java
+++ src/main/java/utfpr/com/produtos/ProdutosApplication.java
@@ -2,8 +2,10 @@ package utfpr.com.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "utfpr.com.produtos.client") // Escaneia o pacote de Feign Clients
 public class ProdutosApplication {
 
 	public static void main(String[] args) {
