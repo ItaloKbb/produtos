3ba1e7d9052cb31df653d5308412a4f3324227e3
MisterAqua <italohyu122@gmail.com>
2024-12-17 04:51:23 -0300

    DTOs de produtos

--- src/main/java/utfpr/com/produtos/dto/request/ProdutoRequestDTO.java
+++ src/main/java/utfpr/com/produtos/dto/request/ProdutoRequestDTO.java
@@ -1,5 +1,16 @@
 package utfpr.com.produtos.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class ProdutoRequestDTO {
-    
+    @NotBlank(message = "O nome do produto é obrigatório")
+    private String nome;
+
+    @NotNull(message = "O preço é obrigatório")
+    private Double preco;
+
+    private String descricao;
 }
--- src/main/java/utfpr/com/produtos/dto/response/ProdutoResponseDTO.java
+++ src/main/java/utfpr/com/produtos/dto/response/ProdutoResponseDTO.java
@@ -1,5 +1,14 @@
 package utfpr.com.produtos.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ProdutoResponseDTO {
-    
+    private String id;
+    private String nome;
+    private Double preco;
+    private String descricao;
+    private String mensagem;
 }
